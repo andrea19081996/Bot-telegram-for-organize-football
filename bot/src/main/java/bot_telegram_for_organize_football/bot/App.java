458bdc028c4d7e90e42f79d96eb707db252b8fe1
Unknown <andrea19081996@gmail.com>
2019-09-20 13:17:33 +0200

    Create a maven project with import telegram libraries

---
+++ bot/src/main/java/bot_telegram_for_organize_football/bot/App.java
@@ -0,0 +1,13 @@
+package bot_telegram_for_organize_football.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
