156168a0eb29fddabd00917abf9c0a7f07350e99
Unknown <andrea19081996@gmail.com>
2019-10-02 19:24:58 +0200

    correct code

--- src/main/java/bot_telegram_for_organize_football/bot/Singol_match.java
+++ src/main/java/bot_telegram_for_organize_football/bot/Singol_match.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:bot/src/main/java/bot_telegram_for_organize_football/bot/Singol_match.java
 package bot_telegram_for_organize_football.bot;
 
 import java.util.Date;
@@ -35,41 +34,3 @@ public class Singol_match {
 	
 	
 }
-=======
-package bot_telegram_for_organize_football.bot;
-
-import java.util.Date;
-
-public class Singol_match {
-	private String date;
-	private String date_time;
-	
-	public Singol_match(String date, String date_time) {
-		this.date=date;
-		this.date_time=date_time;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
-	public String getDate_time() {
-		return date_time;
-	}
-
-	public void setDate_time(String date_time) {
-		this.date_time = date_time;
-	}
-	
-	public void clear() {
-		this.date=null;
-		this.date_time=null;
-	}
-	
-	
-}
->>>>>>> master:src/main/java/bot_telegram_for_organize_football/bot/Singol_match.java
